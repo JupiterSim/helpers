a62e44142c4f17837298ad75b09df8541e00d14f
Andrés Castellanos <andres.cv@galileo.edu>
2020-06-13 18:45:02 -0600

    docs: add api documentation

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export { constants } from './constants';
-export { atof, atoi } from './helpers';
-export { floatToBits, bitsToFloat } from './helpers';
-export { asWord, asUWord, asHalf, asUHalf, asByte, asUByte } from './helpers';
-export { signExtend, signExtendByte, signExtendHalf } from './helpers';
-export { cleanEscapes } from './helpers';
-export { getRegisterNumber } from './helpers';
-export { between, validByte, validHalf, validWord } from './helpers';
